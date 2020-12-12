77268af11fa2cc6cb6c29f4025b7a13a98f37193
Naufal Afif <naufalafif58@gmail.com>
2020-12-12 22:56:21 +0700

    add pipeline control api

--- app/src/api/api.py
+++ app/src/api/api.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from app.src.api.root import api as root
 from app.src.api.sentiment import api as sentiment
+from app.src.api.control import api as control
 
 router = APIRouter()
 
 router.include_router(root.router, prefix='')
 router.include_router(sentiment.router, prefix='/sentiment')
+router.include_router(control.router, prefix='/control')
